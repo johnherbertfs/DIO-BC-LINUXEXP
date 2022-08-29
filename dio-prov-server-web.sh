c76505b1a7dc02983326f65d7853059b37efd5a7
johnherbertfs <91393029+johnherbertfs@users.noreply.github.com>
2022-08-28 23:41:36 -0300

    script dio provisionamento web
    Script dio provisionamento web desafio.

---
+++ dio-prov-server-web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "===== inicio do script para dio linux experience - web ======"
+
+echo "atualizando repositorios locais..."
+
+apt-get update
+
+echo "instalando apache2..."
+
+apt-get install apache2 -y
+
+#  o procedimento de copia do site da amazon nao foi feita pois nao tenho um site pronto
+
+echo "===== fim do script dio ====="
